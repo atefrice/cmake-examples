bf9d858849b1e5c5893c471a51ff4cca637dfa08
yb <atefrice@126.com>
2022-07-14 19:51:18 +0800

    调用boost第三方库 split

--- 01-basic/H-third-party-library/main.cpp
+++ 01-basic/H-third-party-library/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <boost/shared_ptr.hpp>
 #include <boost/filesystem.hpp>
+#include <boost/algorithm/string.hpp>
+
+std::vector<std::string> split(std::string s, std::string delim) {
+  int i = 1;
+  std::vector<std::string> result;
+
+  boost::split(result, s, boost::is_any_of(delim.c_str()));
+
+  std::cout << "NEMA FULL: " << s << "\n";
+  for (const auto& item : result) {
+	  std::cout << "NEMA DELIM[" << i++ << "]:" << item << "\n";
+  }
+
+  return result;
+}
+
 
 int main(int argc, char *argv[])
 {
+    std::string s = "$GPCHC,0,766.08,0.00,-0.63,-0.73,0.38,-0.04,0.08,0.0127,-0.0111,0.9991,0.00000000,0.00000000,0.00,0.000,0.000,0.000,0.000,0,0,01,0,2*5A";
+    std::string delm =",*";
+
+    split(s, delm);		
+
     std::cout << "Hello Third Party Include!" << std::endl;
 
     // use a shared ptr
