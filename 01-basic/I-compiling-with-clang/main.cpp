0b66f80cc62e9bb158bac5b9aa5e855d30323a72
yb <atefrice@126.com>
2022-07-26 21:16:49 +0800

    尝试弄清cmake的预处理编译链接选项

--- 01-basic/I-compiling-with-clang/main.cpp
+++ 01-basic/I-compiling-with-clang/main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
+#ifdef CHINA
+std::string country = "china";
+#else
+std::string country = "english";
+#endif
+
+// will error, because not in preprocessor
+std::string life22 = LIFE;
+
+#ifdef LIFE
+std::string life = LIFE;
+#else
+std::string life = "no_life";
+#endif
+
+int robot_type = ROBOT_TYPE;
+
 int main(int argc, char *argv[])
 {
-   std::cout << "Hello CMake!" << std::endl;
+   std::cout << "Hello CMake!  ---->" <<  country << " ---> "  << life << std::endl;
+   std::cout << robot_type << std::endl;
    return 0;
 }
